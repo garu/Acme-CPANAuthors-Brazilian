49bdd103783556a20e7d8389d20ff873b73085e5
Breno G. de Oliveira <garu@cpan.org>
2015-04-02 23:18:39 -0300

    bumping up copyright year

--- lib/Acme/CPANAuthors/Brazilian.pm
+++ lib/Acme/CPANAuthors/Brazilian.pm
@@ -149,7 +149,7 @@ Breno G. de Oliveira E<lt>garu at cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2008-2014 by Breno G. de Oliveira.
+Copyright (C) 2008-2015 by Breno G. de Oliveira.
 
 This program is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
